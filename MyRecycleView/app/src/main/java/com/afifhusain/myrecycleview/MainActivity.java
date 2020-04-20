a7988560e690cb20f48aad04b25435b0c4e8af4b
user <afifhusain2014@gmail.com>
2020-04-20 16:51:37 +0700

    newcmt

---
+++ MyRecycleView/app/src/main/java/com/afifhusain/myrecycleview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.afifhusain.myrecycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.afifhusain.myrecycleview.adapter.ListHeroAdapter;
+import com.afifhusain.myrecycleview.model.Hero;
+import com.afifhusain.myrecycleview.model.HeroesData;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvHeroes = findViewById(R.id.rv_heroes);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
+
+    }
+
+    private void showRecyclerList(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+    }
+}
